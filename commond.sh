ebf9270faa5754ce1c10057ed9743609a0377a77
Hanumant Thakare <thakare2912@gmail.com>
2026-05-16 22:26:08 +0530

    Refactor commond.sh to use Docker for services
    Removed virtual environment setup and Kafka producer/consumer commands. Added Docker commands to run services and submit Spark jobs.

--- commond.sh
+++ commond.sh
@@ -1,24 +1,11 @@
-# Create virtual environment
-python3 -m venv venv
+docker compose up -d 
+install python 3.10.11
+pip install -r requirements.txt
 
-# Activate virtual environment
-source venv/bin/activate  # On Unix/macOS
-# OR
-venv\Scripts\activate     # On Windows
+cd src/kafka/producer
+python transaction_producer.py
 
-# run the kafka producer
-cd src\kafka\producers
-python product_producer.py
-
-# run the kafka consumer 
-cd src\kafka\consumers
+cd src/kafka/consumer
 python consumer.py
 
-# run the  spark code 
-docker exec -it smart_retail_fraud_trend_pipeline-spark-master-1 /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 /opt/spark/jobs/spark.py
-
-
-docker exec -it smart_retail_fraud_trend_pipeline-spark-master-1 /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 /opt/spark/jobs/spark.py
-
-docker exec -it smart_retail_fraud_trend_pipeline-spark-master-1 /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --jars /opt/spark/jars/hadoop-aws-3.3.4.jar,/opt/spark/jars/aws-java-sdk-bundle-1.12.262.jar --driver-class-path /opt/spark/jars/hadoop-aws-3.3.4.jar:/opt/spark/jars/aws-java-sdk-bundle-1.12.262.jar /opt/spark/jobs/spark.py
-#then load data to snowflake using airflow 
+docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --conf spark.jars.ivy=/tmp/.ivy --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 /opt/spark/jobs/spark.py
